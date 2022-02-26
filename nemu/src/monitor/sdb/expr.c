4cc375948359555d7a95a5a4400e79c5a18cd511
220046-Wang Kai <wkzcml@mail.ustc.edu.cn>
2022-02-26 20:10:18 +0800

    finish extend-expression

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,16 +1,17 @@
 #include <isa.h>
-
+#include <string.h>
+#include <memory/vaddr.h>
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
 #include <regex.h>
-#define MAX_TOKEN_BUFF 31
 
 enum {
   TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
-  TK_DE, TK_HEX,
+  TK_HEX, TK_DE, TK_REG, TK_NE, TK_AND,
+  TK_NEG, TK_DEREF, TK_POS, 
 };
 
 static struct rule {
@@ -22,20 +23,24 @@ static struct rule {
    * Pay attention to the precedence level of different rules.
    */
 
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ},        // equal
-  {"0[x|X][0-9A-Fa-f]+", TK_HEX}, 
-                        // hexadecimal number
-  {"[0-9]+", TK_DE},    // decimal
-  {"-", '-'},           // subtract
-  {"\\*", '*'},         // multiply
-  {"/", '/'},           // divide
-  {"\\(", '('},         // left parenthesis
-  {"\\)", ')'},         // right parenthesis
+  {" +", TK_NOTYPE},                // spaces
+  {"\\+", '+'},                     // plus
+  {"==", TK_EQ},                    // equal
+  {"0[x|X][0-9A-Fa-f]+", TK_HEX}, 	// hexadecimal-number 
+	{"[0-9]+", TK_DE},		            // decimal
+	{"\\$[\\$a-z0-9]{2,3}", TK_REG},	// regs' name
+  {"==", TK_EQ},                    // ==
+  {"!=", TK_NE},                    // !=
+  {"&&", TK_AND},                   // &&
+  {"-", '-'},                       // substract
+  {"/", '/'},                       // divide
+  {"\\*", '*'},                     // multiply  
+  {"\\(", '('},
+  {"\\)", ')'},  
 };
 
 #define NR_REGEX ARRLEN(rules)
+#define MAX_BUF 31
 
 static regex_t re[NR_REGEX] = {};
 
@@ -89,24 +94,20 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          case TK_NOTYPE:
-            break;                // ignore spaces
-          case TK_DE: case TK_HEX:
-            int ignoreLen = 0;    // exceed the buff limit
-            
-            if(substr_len > MAX_TOKEN_BUFF) {
-              ignoreLen = substr_len - MAX_TOKEN_BUFF;
-            }
-
-            int strIndex = 0;
-            int index = position + ignoreLen - substr_len;
-            while(index < position) {
-              tokens[nr_token].str[strIndex++] = e[index++];
-            }
-            tokens[nr_token].str[strIndex] = '\0';
-
-          default:
-            tokens[nr_token++].type = rules[i].token_type;
+        case TK_NOTYPE:
+          // ignore spaces
+          break;
+        case TK_HEX:
+        case TK_DE:
+        case TK_REG:
+          // 超过buf长度忽略前几位
+          if (substr_len > MAX_BUF) {
+            substr_start += (substr_len - MAX_BUF);
+            substr_len = MAX_BUF;
+          }
+          strncpy(tokens[nr_token].str, substr_start, substr_len);
+        default:
+          tokens[nr_token++].type = rules[i].token_type;
         }
 
         break;
@@ -122,157 +123,229 @@ static bool make_token(char *e) {
   return true;
 }
 
-/* get parentheses pairs  */
-int stack[32], top = -1;
-const int NOT_PAIR = -2;
-int parentheses_pair[32];
+/* 利用栈进行括号匹配 */
+int pairs[MAX_BUF];            // 储存括号对
+int stack[MAX_BUF], top = -1; // 栈
 
-void search_parentheses( bool *success ) {
-  //initial
-  top = -1;
-  // search 
+
+void make_parentheses(bool *success) {
   for(int i = 0; i < nr_token; ++i) {
-    if (tokens[i].type == '(') {
+    switch (tokens[i].type) {
+    case '(':
       stack[++top] = i;
-    } else if (tokens[i].type == ')') {
-      parentheses_pair[i] = stack[top];
-      parentheses_pair[stack[top--]] = i;
-    } else {
-      parentheses_pair[i] = NOT_PAIR;
+      break;
+    case ')':
+      if (top == -1) {
+        success = false;
+        return;
+      }
+      pairs[i] = stack[top];
+      pairs[stack[top--]] = i;
+      break;
+    default:
+      continue;
     }
   }
-  // judge
-  if(top > -1) *success = false;
-}
-
-bool check_parentheses( int p, int q ) {
-  return q == parentheses_pair[p];
-}
-
-int get_main_op (int p, int q) {
-  
-  int ret = -1, pos = p;
-
-  while ( pos <= q ) {
-    switch ( tokens[pos].type ) {
-      case '+': case '-':
-        return pos;
-      case '*': case '/':
-        ret = ret == -1? pos++: -1;
-        break;
-      case '(':
-        pos = parentheses_pair[p] + 1;
-        break;
-      default: ++pos;
-    }
+  if (top != -1) {
+    success = false;
   }
-
-  return ret;
 }
 
-int minus_times = 0;
+bool check_parentheses(int p, int q) {
+  return pairs[p] == q;
+}
 
-word_t get_number(int p) {
+
+/* 取得token中数字值 */
+word_t get_num(int p, bool* success) {
   word_t ret = 0;
   switch (tokens[p].type) {
+    case TK_DE:
+      for (int i = 0; tokens[p].str[i] != '\0'; ++i) {
+        ret = ret * 10 + tokens[p].str[i] - '0';
+      }
+      return ret;
+    case TK_HEX:
+      for (int i = 0; tokens[p].str[i] != '\0'; ++i) {
+        char c = tokens[p].str[i];
+        if (c >= '0' && c <= '9') {
+          ret = ret * 16 + c - '0';
+        } else if (c >= 'a' && c <= 'f') {
+          ret = ret * 16 + c - 'a' + 10;
+        } else if (c >= 'A' && c <= 'F') {
+          ret = ret * 16 + c - 'A' + 10;
+        }
+      }
+      return ret;
+    case TK_REG:
+      return isa_reg_str2val(tokens[p].str, success);
+  }
+  *success = false;
+  return 0;
+}
+
+/* 判断某个运算符是否为单目运算符 */
+bool isUnaryOp(int p) {
+  if (p == 0) {
+    return true;
+  }
+  switch (tokens[p - 1].type) {
   case TK_DE:
-    for(int i = 0; tokens[p].str[i] != '\0'; ++i) {
-      ret = ret * 10 + tokens[p].str[i] - '0';
-    }
-    break;
-  
   case TK_HEX:
-    for(int i = 2; tokens[p].str[i] != '\0'; ++i) {
-      char c = tokens[p].str[i];
-      if (c >= '0' && c <= '9') {
-        ret = ret * 16 + c - '0';
-      } else if (c >= 'a' && c <= 'f') {
-        ret = ret * 16 + c - 'a' + 10;
-      } else {
-        ret = ret * 16 + c - 'A' + 10;
-      }
-    }
-    break;
+  case TK_REG:
+  case ')':
+    return false;
   }
-  return ret;
-} 
 
-word_t eval(int p, int q, bool *success) {
-  if ( p > q ) {
-    // error
+  return true;
+}
+
+/* 得到主操作符位置 */
+#define MAX_PRI 3
+#define NOT_FOUND 404
+int getMainOp(int p, int q) {
+  
+  int priority[MAX_PRI];
+  for (int i = 0; i < MAX_PRI; ++i) {
+    priority[MAX_PRI] = NOT_FOUND;
+  } 
+
+  int index = p;
+  while (index <= q) {
+    switch (tokens[index].type) {
+
+    case '(':
+      // 跳过括号
+      index = pairs[index] + 1;
+      break;
+    case TK_AND:
+      // 优先级最低
+      return p;
+    case TK_EQ: case TK_NE:
+      // 优先级次低
+      if (priority[0] == NOT_FOUND) {
+        priority[0] = index;
+      }
+      ++index;
+      break;
+    case '+': case '-':
+      if (priority[1] == NOT_FOUND) {
+        priority[1] = index;
+      }
+      ++index;
+      break;
+    case '*': case '/':
+      // 优先级最高
+      if (priority[2] == NOT_FOUND) {
+        priority[2] = index;
+      }
+      ++index;
+      break;
+	default: :
+      ++index; 
+      break;
+
+    }               
+  }
+
+  for(int i = 0; i < MAX_PRI; ++i) {
+    if (priority[i] != NOT_FOUND) {
+      return priority[i];
+    }
+  }
+
+  return NOT_FOUND;
+}
+
+word_t eval(int p, int q, bool* success) {
+  if (p > q) {
+
     *success = false;
     return 0;
 
-  } else if ( p == q ) {
-    // get a num
-    word_t ret = get_number(p);
-    
-    if (minus_times > 0) {
-      if (minus_times % 2) {
-        minus_times = 0;
-        return -ret;
-      }
-      minus_times = 0;
-    }
+  } else if (p == q) {
 
-    return ret;
-  } else if ( check_parentheses(p, q) ) {
-    // surrounded by a matched pair of parentheses.
+    return get_num(p, success);
+
+  } else if (check_parentheses(p, q)) {
+
     return eval(p + 1, q - 1, success);
-  } else if (tokens[p].type == '-') {
-    ++minus_times;
-    return eval(p + 1, q, success);
-  }
-  
-  int op_pos = get_main_op(p, q);
-  
-  if(op_pos == -1) {
+
+  } else if (getMainOp(p, q) != NOT_FOUND) {
+    
+    int mainPos = getMainOp(p, q);
+    word_t left = eval(p, mainPos - 1, success);
+    word_t right = eval(mainPos + 1, q, success);
+
+    if (*success == false) {
+      return 0;
+    }
+
+    switch (tokens[mainPos].type) {
+      case '+': return left + right;
+      case '-': return left - right;
+      case '*': return left * right;
+      case '/': return left / right;
+      case TK_EQ: return left == right;
+      case TK_NE: return left != right;
+      case TK_AND: return left && right;
+	  default: assert(0);				   
+    }
+
     *success = false;
     return 0;
-  }
-  
-  word_t left = eval(p, op_pos - 1, success);
-  word_t right = eval(op_pos + 1, q, success);
 
-  if (minus_times > 0) {
-    if (minus_times % 2) {
-      left = -left;
+  } else if (tokens[p].type == TK_NEG) {
+
+    return -eval(p + 1, q, success);
+
+  } else if (tokens[p].type == TK_POS) {
+    
+    return eval(p + 1, q, success);
+
+  } else if (tokens[p].type == TK_DEREF) {
+
+    word_t addr = eval(p + 1, q, success);
+    
+    if (*success == false) {
+      return 0;
     }
-    minus_times = 0;
+    return  vaddr_read(addr, 1);
+
   }
 
-  if(*success == false) {
-    return 0;
-  }
-
-  switch ( tokens[op_pos].type ) {
-    case '+': return left + right;
-    case '-': return left - right;
-    case '*': return left * right;
-    case '/':
-      if(right == 0) {
-        *success = false;
-        return 0;
-      } else {
-        return left / right;
-      }
-  }
-
+  *success = false;
   return 0;
 }
 
 word_t expr(char *e, bool *success) {
-  
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  search_parentheses( success );
-  
-  if( *success  == false ) return 0;
-  
-  return eval(0, nr_token - 1, success); 
+  make_parentheses(success);
 
+  if (!*success) {
+    printf("Wrong expression!\n");
+    return 0;
+  }
+
+  // 预处理，提前发现单目运算符
+  for (int i = 0; i < nr_token; ++i) {
+    if (tokens[i].type == '*' && isUnaryOp(i)) {
+      tokens[i].type = TK_DEREF;
+    } else if (tokens[i].type == '-' && isUnaryOp(i)) {
+      tokens[i].type = TK_NEG;
+    } else if (tokens[i].type == '+' && isUnaryOp(i)) {
+      tokens[i].type = TK_POS;
+    }
+  }
+  
+  word_t ret = eval(0, nr_token - 1, success);
+  if (*success == true) {
+    return ret;
+  }
+  return 0;
 }
