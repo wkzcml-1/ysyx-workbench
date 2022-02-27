181fbd1b0957488c8a14830d4fb275ea79ff79e4
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-27 19:41:41 +0800

    >  compile NEMU ysyx_220046 王凯 Linux Kai-PC 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:41:40 up  4:32,  0 users,  load average: 0.59, 0.14, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -106,6 +106,7 @@ static bool make_token(char *e) {
             substr_len = MAX_BUF;
           }
           strncpy(tokens[nr_token].str, substr_start, substr_len);
+          tokens[nr_token].str[substr_len + 1] = '\0';
         default:
           tokens[nr_token++].type = rules[i].token_type;
         }
